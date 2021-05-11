df1d2e7ba7e0355811c68221013571ce53d89b0f
Pranjal Goyal <id4pranjal@gmail.com>
2021-05-12 00:35:22 +0530

    added css to character

--- charcter/script.js
+++ charcter/script.js
@@ -7,18 +7,24 @@ window.addEventListener("keydown", function(event) {
     }
     
     console.log(event);
+    document.querySelector(".discrip").innerHTML = "Keycode of Key is "+event.keyCode;
     document.querySelector(".prop-1 span").innerHTML = event.code;
     document.querySelector(".prop-2 span").innerHTML = Math.round(event.timeStamp) + " ms";
     document.querySelector(".prop-3 span").innerHTML = (event.key).charCodeAt(0).toString(2);
 });
 document.querySelector(".mode").addEventListener("click", function() {
-    document.querySelector("body").classList.toggle("dark-mode");
-    var text = document.querySelector(".mode");
-    if(text.innerHTML == "Dark Mode") {
-        text.innerHTML = "Light Mode"
+    var body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+    var chk = body.classList.contains("dark-mode")
+    if (chk === true) {
+        document.querySelector("i").classList.remove("fa-moon");
+        document.querySelector("i").classList.add("fa-sun");
     }
     else {
-        text.innerHTML = "Dark Mode"
+        document.querySelector("i").classList.remove("fa-sun");
+        document.querySelector("i").classList.add("fa-moon");
     }
+
+
 });
 
