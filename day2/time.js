be6f12b00487d136fdac381aacef17417c7df66b
Pranjal Goyal <id4pranjal@gmail.com>
2021-06-06 23:56:24 +0530

    changed header

--- day2/time.js
+++ day2/time.js
@@ -1,11 +1,23 @@
+function london() {
+    var hourHand = document.querySelector(".london .hour");
+    var minHand = document.querySelector(".london .min");
+    var secHand = document.querySelector(".london .second");
+    setTime(-4, -30, 0, hourHand, secHand, minHand);
+    
+}
 
-
-var hourHand = document.querySelector(".second");
-setInterval(function () {
+function setTime(chour, cMin, Csec, hourHand, minHand, secHand) {
     var curDate = new Date();
-    var hour = curDate.getHours();
-    var min = curDate.getMinutes();
-    var sec = curDate.getSeconds();
-    hourHand.style.transform = "rotate("+sec+"deg);"; 
-    console.log("rotate("+sec+"deg);");
-}, 1000);
+    var hour = curDate.getHours() + chour;
+    var hourDeg = ((hour/12) * 360) + 90;
+    var min = curDate.getMinutes() + cMin;
+    var minDeg = ((min/60) * 360) + 90;
+    var sec = curDate.getSeconds() + Csec;
+    var secDeg = ((sec/60) * 360) + 90;
+    hourHand.style.transform = "rotate("+hourDeg+"deg)";
+    minHand.style.transform = "rotate("+minDeg+"deg)"; 
+    secHand.style.transform = "rotate("+secDeg+"deg)";
+    console.log(hourDeg);
+}
+setInterval(london, 1000);
+console.log(document.querySelector(".london .hour"));
