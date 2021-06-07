b3a93cbbbb97fdb075ca8ef95d9e37e6cdf5a443
Pranjal Goyal <id4pranjal@gmail.com>
2021-06-07 19:21:24 +0530

    made all clocks

--- day2/time.js
+++ day2/time.js
@@ -2,8 +2,49 @@ function london() {
     var hourHand = document.querySelector(".london .hour");
     var minHand = document.querySelector(".london .min");
     var secHand = document.querySelector(".london .second");
-    setTime(-4, -30, 0, hourHand, secHand, minHand);
+    var time = document.querySelector(".london .time");
+    setTime(-4, -30, 0, hourHand, secHand, minHand, time);
     
+}
+function india() {
+    var hourHand = document.querySelector(".india .hour");
+    var minHand = document.querySelector(".india .min");
+    var secHand = document.querySelector(".india .second");
+    var time = document.querySelector(".india .time");
+    setTime( 0, 0, 0, hourHand, secHand, minHand, time);
+
+}
+function us() {
+    var hourHand = document.querySelector(".us .hour");
+    var minHand = document.querySelector(".us .min");
+    var secHand = document.querySelector(".us .second");
+    var time = document.querySelector(".us .time");
+    setTime( -9, -30, 0, hourHand, secHand, minHand, time);
+
+}
+function japan() {
+    var hourHand = document.querySelector(".japan .hour");
+    var minHand = document.querySelector(".japan .min");
+    var secHand = document.querySelector(".japan .second");
+    var time = document.querySelector(".japan .time");
+    setTime( 3, 30, 0, hourHand, secHand, minHand, time);
+
+}
+function russia() {
+    var hourHand = document.querySelector(".russia .hour");
+    var minHand = document.querySelector(".russia .min");
+    var secHand = document.querySelector(".russia .second");
+    var time = document.querySelector(".russia .time");
+    setTime( -2, -30, 0, hourHand, secHand, minHand, time);
+
+}
+function france() {
+    var hourHand = document.querySelector(".france .hour");
+    var minHand = document.querySelector(".france .min");
+    var secHand = document.querySelector(".france .second");
+    var time = document.querySelector(".france .time");
+    setTime( -3, -30, 0, hourHand, secHand, minHand, time);
+
 }
 
 function setTime(chour, cMin, Csec, hourHand, minHand, secHand) {
@@ -20,4 +61,9 @@ function setTime(chour, cMin, Csec, hourHand, minHand, secHand) {
     console.log(hourDeg);
 }
 setInterval(london, 1000);
+setInterval(india, 1000);
+setInterval(us, 1000);
+setInterval(japan, 1000);
+setInterval(russia, 1000);
+setInterval(france, 1000);
 console.log(document.querySelector(".london .hour"));
