08bafe0c3d9d34a1801c011f1238cd890f10ea84
Pranjal Goyal <id4pranjal@gmail.com>
2021-08-18 21:52:36 +0530

    made comickon webpage

--- handgame/script.js
+++
@@ -1,7 +0,0 @@
-// Animation SVG
-var path = document.querySelectorAll("#logo path");
-
-for (i=0; i <= path.length ; i++) {
-    var pathLenth = path[i].getTotalLength();
-    console.log(pathLenth);
-}
